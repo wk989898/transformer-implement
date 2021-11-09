8dda24ce191c423810bef4bf857769ccf6f51244
wk989898 <wangkun989898@gmail.com>
2021-11-09 17:13:05 +0800

    refactor

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import DataLoader, Dataset
-from transformer import Transformer
+from transformer import Transformer, compute_loss
 from datasets import load_dataset
 import os
 from tqdm import tqdm
@@ -31,7 +31,7 @@ def batch_iterator(dataset):
 
 def preprocess(dataset, file='tokenizer.json', prefix='', tokenizer=None):
     if tokenizer is None:
-        tokenizer = loadTokenzier(dataset, file)
+        tokenizer = loadTokenzier(dataset, file, prefix=prefix)
     dataset = map(lambda x: (tokenizer.encode(
         x['cs']), tokenizer.encode(x['en'])), dataset)
     dataset = filter(lambda x: 2 < len(
@@ -40,8 +40,8 @@ def preprocess(dataset, file='tokenizer.json', prefix='', tokenizer=None):
     return list(dataset), tokenizer
 
 
-def loadTokenzier(dataset, file='tokenizer.json'):
-    if not os.path.exists(file):
+def loadTokenzier(dataset, file='tokenizer.json', prefix=''):
+    if not os.path.exists(prefix+file):
         print('train Tokenzier')
         tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
         tokenizer.enable_padding()
@@ -57,12 +57,12 @@ def loadTokenzier(dataset, file='tokenizer.json'):
         #     ],
         # )
         trainer = BpeTrainer(
-            vocab_size=10000,
+            # vocab_size=10000,
             special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
         tokenizer.train_from_iterator(batch_iterator(dataset), trainer=trainer)
-        tokenizer.save(file)
+        tokenizer.save(prefix+file)
     else:
-        tokenizer = Tokenizer.from_file(file)
+        tokenizer = Tokenizer.from_file(prefix+file)
 
     return tokenizer
 
@@ -75,41 +75,36 @@ def update_lr(optimizer, step, args):
 
 
 def collate_fn(batch):
-    max_cs_len, max_en_len = 0, 0
-    for cs, en in batch:
-        max_cs_len = max(max_cs_len, len(cs.tokens))
-        max_en_len = max(max_en_len, len(en.tokens))
     cs_batch, en_batch = [], []
-    cs_mask, en_mask = [], []
     for cs, en in batch:
-        mask_idx = len(en.tokens)-1
-        cs.pad(max_cs_len, pad_token='[MASK]')
-        en.pad(max_en_len, pad_token='[MASK]')
+        cs.pad(args.max_len, pad_id=0)
+        en.pad(args.max_len, pad_id=0)
         cs_batch.append(cs.ids)
-        cs_mask.append(cs.attention_mask)
         en_batch.append(en.ids)
-        en_attention_mask = list(en.attention_mask)
-        en_attention_mask[mask_idx] = 0
-        en_mask.append(en_attention_mask)
-    return cs_batch, en_batch, cs_mask, en_mask
+    return cs_batch, en_batch
 
 
 def main(args):
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
     dataset = load_dataset(
         'wmt16', 'cs-en', split='train').to_dict()['translation']
     train_data, tokenizer = preprocess(dataset)
     args.vocab_dim = tokenizer.get_vocab_size()
+    args.pad_idx = 0
 
-    # train_data = MyDataSet(dataset)
     train_data = DataLoader(train_data, batch_size=args.batch_size,
-                            num_workers=8, pin_memory=True, collate_fn=collate_fn)
+                            num_workers=8, collate_fn=collate_fn)
 
     model = Transformer(args.vocab_dim, args.dim,
-                        args.atten_dim, recycle=7).to(device)
+                        args.atten_dim, pad_idx=args.pad_idx, recycle=7)
+    device = 'cpu'
+    if torch.cuda.is_available():
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+        device = 'cuda'
+        model = torch.nn.DataParallel(model).to(device)
     print(f'args:{args}')
+
     optimizer = torch.optim.Adam(
         model.parameters(), betas=[0.9, 0.98], eps=1e-9)
 
@@ -118,23 +113,24 @@ def main(args):
         total_loss = 0
         total_acc = 0
         total_n = 0
-        for cs, en, cs_mask, en_mask in tqdm(train_data):
+        for cs, en in tqdm(train_data):
             cs, en = torch.tensor(cs).to(device), torch.tensor(en).to(device)
-            cs_mask, en_mask = torch.tensor(cs_mask).to(
-                device).detach(), torch.tensor(en_mask).to(device).detach()
-            label = en.clone().detach()
-            pred = model(cs, en, cs_mask, en_mask)
-            loss, acc = model.compute_loss(pred, label)
+            label = en[..., 1:].clone()
+            en = en[..., :-1]
+            pred = model(cs, en)
+            loss, acc = compute_loss(
+                pred, label, pad_idx=args.pad_idx, vocab_dim=args.vocab_dim)
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
             update_lr(optimizer, iter+1, args)
 
             total_loss += loss.item()
-            total_acc += acc
-            total_n += label.nonzero().sum()
+            total_acc += acc.item()
+            total_n += label.ne(args.pad_idx).sum().item()
+        lr = optimizer.param_groups[0]['lr']
         print(
-            f'iter:{iter} loss:{total_loss/total_n:.2f} acc:{total_acc/total_n:.2f}')
+            f'iter:{iter} loss:{total_loss/total_n} acc:{total_acc/total_n} total_words:{total_n} lr:{lr}')
 
     validation = load_dataset(
         'wmt16', 'cs-en', split="validation").to_dict()['translation']
@@ -143,23 +139,22 @@ def main(args):
 
     model.eval()
     with torch.no_grad():
-        for cs, en, cs_mask, en_mask in tqdm(validation_data):
+        for cs, en in tqdm(validation_data):
             cs, en = torch.tensor(cs).to(device), torch.tensor(en).to(device)
-            cs_mask, en_mask = torch.tensor(cs_mask).to(
-                device).detach(), torch.tensor(en_mask).to(device).detach()
-            label = en.clone().detach()
-            pred = model(cs, en, cs_mask, en_mask)
+            label = en[..., 1:].clone()
+            en = en[..., :-1]
+            pred = model(cs, en)
             non_pad_mask = label.ne(0)
             p = torch.argmax(pred, dim=-1)
-            gt=label
-            assert p.shape == gt.shape,f'pred shape:{p.shape} and gt shape:{gt.shape}'
-            acc = p.eq(label).masked_select(non_pad_mask).sum().item()
-            
-            total_n += label.nonzero().sum()
-            total_acc += acc
+            gt = label
+            assert p.shape == gt.shape, f'pred shape:{p.shape} and gt shape:{gt.shape}'
+            acc = p.eq(gt).masked_select(non_pad_mask).sum()
+
+            total_n += non_pad_mask.sum().item()
+            total_acc += acc.item()
     print(f'acc:{total_acc/total_n:.2f}')
 
-    torch.save(model, args.save_path)
+    torch.save(model.state_dict(), args.save_path)
 
 
 if __name__ == '__main__':
@@ -169,7 +164,7 @@ if __name__ == '__main__':
     parse.add_argument('--epoch', type=int, default=1000)
     parse.add_argument('--warm_step', type=int, default=4000)
     parse.add_argument('--max_len', type=int, default=40)
-    parse.add_argument('--batch_size', type=int, default=256)
+    parse.add_argument('--batch_size', type=int, default=512)
     parse.add_argument('--seed', type=int)
     parse.add_argument('--save_path', type=str,
                        default='model.pt', help='specify path to save model')
--- transformer.py
+++ transformer.py
@@ -1,55 +1,70 @@
 from operator import le
 import torch
+from torch._C import device
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange, repeat
 
 
+def compute_loss(pred: torch.Tensor, label: torch.Tensor, pad_idx=0, smoothing=False, vocab_dim=10000):
+    non_pad_mask = label.ne(pad_idx)
+
+    p = torch.argmax(pred, dim=-1)
+    gt = label
+    assert p.shape == gt.shape, f'pred shape:{p.shape} and gt shape:{gt.shape}'
+    acc = p.eq(gt).masked_select(non_pad_mask).sum()
+
+    if smoothing:
+        label = F.one_hot(label, num_classes=vocab_dim)
+        eps = 0.1
+        label = label * (1 - eps) + (1 - label) * \
+            eps / (vocab_dim - 1)
+        log_prb = F.log_softmax(pred, dim=1)
+        loss = -(label * log_prb).sum(dim=-1)
+        loss = loss.masked_select(non_pad_mask).sum()
+    else:
+        pred = pred.view(-1, pred.size(-1))
+        label = label.view(-1)
+        # Specifies a target value that is ignored and does not contribute to the input gradient.
+        loss = F.cross_entropy(pred, label, ignore_index=pad_idx)
+
+    return loss, acc
+
+
 class Transformer(nn.Module):
-    def __init__(self, vocab_dim, dim, atten_dim, recycle=1):
+    def __init__(self, vocab_dim, dim, atten_dim, pad_idx=0, recycle=1):
         super().__init__()
-        self.encoder = Encoder(vocab_dim, dim, atten_dim, recycle=recycle)
-        self.decoder = Decoder(vocab_dim, dim, atten_dim, recycle=recycle)
+        self.encoder = Encoder(vocab_dim, dim, atten_dim,
+                               pad_idx=pad_idx, recycle=recycle)
+        self.decoder = Decoder(vocab_dim, dim, atten_dim,
+                               pad_idx=pad_idx, recycle=recycle)
         self.fc = nn.Linear(dim, vocab_dim)
 
-        # Specifies a target value that is ignored and does not contribute to the input gradient.
-        self.criterion = nn.CrossEntropyLoss(reduction='sum',ignore_index=0)
-        self.vocab_dim=vocab_dim
+        self.pad_idx = pad_idx
 
-    def forward(self, inputs, outputs, input_mask=None, output_mask=None):
+    def generate_mask(self, inputs, outputs):
+        input_mask = inputs.ne(self.pad_idx).unsqueeze(-2).clone()
+        output_mask = outputs.ne(self.pad_idx).unsqueeze(-2).clone()
+        out_len = outputs.shape[-1]
+        output_mask = output_mask & (
+            1-torch.triu(torch.ones((1, out_len, out_len), device=outputs.device), diagonal=1)).bool()
+        return input_mask, output_mask
+
+    def forward(self, inputs: torch.Tensor, outputs: torch.Tensor):
+        input_mask, output_mask = self.generate_mask(inputs, outputs)
+
         encode = self.encoder(inputs, input_mask)
         decode = self.decoder(encode, outputs, input_mask, output_mask)
 
         out = torch.softmax(self.fc(decode), dim=-1)
         return out
 
-    def compute_loss(self, pred:torch.Tensor, label: torch.Tensor, smoothing=False):
-        non_pad_mask = label.ne(0)
-
-        p = torch.argmax(pred, dim=-1)
-        gt=label
-        assert p.shape == gt.shape,f'pred shape:{p.shape} and gt shape:{gt.shape}'
-        acc = p.eq(label).masked_select(non_pad_mask).sum().item()
-
-        if smoothing:
-            label = F.one_hot(label, num_classes=self.vocab_dim)
-            eps=0.1
-            label = label * (1 - eps) + (1 - label) * eps / (self.vocab_dim - 1)
-            log_prb = F.log_softmax(pred, dim=1)
-            loss = -(label * log_prb).sum(dim=-1)
-            loss = loss.masked_select(non_pad_mask).sum()
-        else:
-            pred = pred.view(-1, pred.size(-1))
-            label = label.view(-1)
-            loss = self.criterion(pred, label)
-
-        return loss, acc
-
 
 class Attention(nn.Module):
-    def __init__(self, dim):
+    def __init__(self, dim, pad_idx):
         super().__init__()
         self.tem = dim**0.5
+        self.pad_idx = pad_idx
 
     def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask=None):
         '''
@@ -58,18 +73,18 @@ class Attention(nn.Module):
         '''
         attn = torch.matmul(q/self.tem, k.transpose(3, 2))
         if mask is not None:
-            mask = rearrange(mask, 'm l -> m () () l')
+            mask = rearrange(mask, 'm l1 l2-> m () l1 l2')
             assert mask.shape[-1] == attn.shape[-1], f'masked_fill same size mask:{mask.shape} attention:{attn.shape}'
-            attn.masked_fill_(mask == 0, -1e9)
+            attn.masked_fill_(mask == self.pad_idx, -1e9)
         attn = torch.softmax(attn, -1)
         return torch.matmul(attn, v)
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, dim, atten_dim, head=8):
+    def __init__(self, dim, atten_dim, pad_idx=0, head=8):
         super().__init__()
         self.head = head
-        self.atten = Attention(atten_dim)
+        self.atten = Attention(atten_dim, pad_idx)
         self.q_trans = nn.Linear(dim, head*atten_dim)
         self.k_trans = nn.Linear(dim, head*atten_dim)
         self.v_trans = nn.Linear(dim, head*atten_dim)
@@ -93,7 +108,7 @@ class MultiHeadAttention(nn.Module):
         atten = rearrange(atten, 'm n l d -> m l (d n)')
         atten = self.fc(atten)
 
-        atten=self.drop(atten)
+        atten = self.drop(atten)
         atten += q
         atten = self.norm(atten)
         return atten
@@ -105,11 +120,11 @@ class FeedForward(nn.Module):
         self.dim = dim
         self.fc1 = nn.Linear(dim, hide_dim)
         self.fc2 = nn.Linear(hide_dim, dim)
-        self.norm=nn.LayerNorm(dim)
+        self.norm = nn.LayerNorm(dim)
 
     def forward(self, x):
-        out=self.fc2(F.relu(self.fc1(x)))
-        out+=x
+        out = self.fc2(F.relu(self.fc1(x)))
+        out += x
         return self.norm(out)
 
 
@@ -132,10 +147,10 @@ class PositionalEncoding(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, vocab_dim, dim, atten_dim, recycle=1):
+    def __init__(self, vocab_dim, dim, atten_dim, pad_idx, recycle=1):
         super().__init__()
-        self.embed = nn.Embedding(vocab_dim, dim)
-        self.MHA = MultiHeadAttention(dim, atten_dim)
+        self.embed = nn.Embedding(vocab_dim, dim, padding_idx=pad_idx)
+        self.MHA = MultiHeadAttention(dim, atten_dim, pad_idx=pad_idx)
         self.PE = PositionalEncoding(dim)
         self.ff = FeedForward(dim)
         self.recycle = recycle
@@ -153,10 +168,10 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
 
-    def __init__(self, vocab_dim, dim, atten_dim, recycle=1):
+    def __init__(self, vocab_dim, dim, atten_dim, pad_idx, recycle=1):
         super().__init__()
-        self.embed = nn.Embedding(vocab_dim, dim)
-        self.MHA = MultiHeadAttention(dim, atten_dim)
+        self.embed = nn.Embedding(vocab_dim, dim, padding_idx=pad_idx)
+        self.MHA = MultiHeadAttention(dim, atten_dim, pad_idx=pad_idx)
         self.PE = PositionalEncoding(dim)
         self.ff = FeedForward(dim)
         self.recycle = recycle
